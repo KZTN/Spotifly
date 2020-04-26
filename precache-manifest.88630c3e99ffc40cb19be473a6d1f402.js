0197dcd00ceb73c87c89f53a3a7ba91c8ac3ef74
KZTN <kakaiocesar2@gmail.com>
2020-04-26 18:45:20 -0300

    Updates

---
+++ precache-manifest.88630c3e99ffc40cb19be473a6d1f402.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf1db6dbeb82329638a233f6a5c163f2",
+    "url": "/Spotifly/index.html"
+  },
+  {
+    "revision": "a99ac773dc5c87d865f8",
+    "url": "/Spotifly/static/css/2.16e1d9ce.chunk.css"
+  },
+  {
+    "revision": "9234ac905945cab32d8c",
+    "url": "/Spotifly/static/css/main.1e38b2dd.chunk.css"
+  },
+  {
+    "revision": "a99ac773dc5c87d865f8",
+    "url": "/Spotifly/static/js/2.78710fb2.chunk.js"
+  },
+  {
+    "revision": "fdd30b2ab44ae896589b798d411d5c2c",
+    "url": "/Spotifly/static/js/2.78710fb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9234ac905945cab32d8c",
+    "url": "/Spotifly/static/js/main.2ced6fdb.chunk.js"
+  },
+  {
+    "revision": "4f703027879b6be4145c",
+    "url": "/Spotifly/static/js/runtime-main.b328638c.js"
+  },
+  {
+    "revision": "145ae1e715558908e29811cba088758e",
+    "url": "/Spotifly/static/media/CircularStd-Bold.145ae1e7.ttf"
+  },
+  {
+    "revision": "9e31161108ad69cc75264c71274e8a58",
+    "url": "/Spotifly/static/media/CircularStd-Book.9e311611.ttf"
+  },
+  {
+    "revision": "9834a799d382c9e7250b67541d3fc034",
+    "url": "/Spotifly/static/media/hearticon.9834a799.svg"
+  },
+  {
+    "revision": "e7a5703bb774ac138d0cbe370ab196d0",
+    "url": "/Spotifly/static/media/home.e7a5703b.svg"
+  },
+  {
+    "revision": "b38d7a1d344574e5e9508c6f87b35ffe",
+    "url": "/Spotifly/static/media/library.b38d7a1d.svg"
+  },
+  {
+    "revision": "ceeb8cd18b9f25fec20a04dcc8d8bfb0",
+    "url": "/Spotifly/static/media/noteicon.ceeb8cd1.svg"
+  },
+  {
+    "revision": "82a32ca05de1c5da6b596f9ed4e7e723",
+    "url": "/Spotifly/static/media/playicon.82a32ca0.svg"
+  },
+  {
+    "revision": "cf7d28b636a9ff79d5a592b941555775",
+    "url": "/Spotifly/static/media/search.cf7d28b6.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Spotifly/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Spotifly/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Spotifly/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Spotifly/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "ba0cc007965a9b148953ac1a9a695d53",
+    "url": "/Spotifly/static/media/threedotsicon.ba0cc007.svg"
+  },
+  {
+    "revision": "659cde52b2227b8772332d1a9b73be52",
+    "url": "/Spotifly/static/media/topbackicon.659cde52.svg"
+  },
+  {
+    "revision": "caae9c376aeff82bae8e5c71771ee2fc",
+    "url": "/Spotifly/static/media/topfowardicon.caae9c37.svg"
+  }
+]);
